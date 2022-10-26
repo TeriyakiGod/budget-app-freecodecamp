25122ed2b77e80ec0d03c2ae8580b98d57c070d3
Kacper Ochnik <36707723+TeriyakiGod@users.noreply.github.com>
2022-10-26 22:48:21 +0000

    Finished exercise

--- main.py
+++ main.py
@@ -3,23 +3,16 @@ import budget
 from budget import create_spend_chart
 from unittest import main
 
-food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = budget.Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
-
-print(food)
-print(clothing)
-
-print(create_spend_chart([food, clothing, auto]))
+food = budget.Category("food")
+entertainment = budget.Category("enterteinment")
+business = budget.Category("business")
+food.deposit(900, "deposit")
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
+print(create_spend_chart([business, entertainment, food]))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
